623babae61b4e7028ccbe5c03e3d1202d4ae18f6
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-14 21:26:23 +0600

    Specializations component added

---
+++ src/Pages/Home/About/Specializations.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import img1 from "../../../assets/icons/development.png";
+import img2 from "../../../assets/icons/code.png";
+import img3 from "../../../assets/icons/graphic-designer.png";
+
+const Specializations = () => {
+    return (
+        <div>
+            <div className='text-center'>
+                <p className='font-bold text-xl mb-3'> My <span className='text-gradient'>Specializations</span></p>
+            </div>
+            <div className='my-10'>
+                <div className='flex flex-col gap-10 lg:flex-row justify-center items-center'>
+                    <div className='border hover:border-secondary border-accent 
+                    hover:transition-all hover:ease-linear
+                    hover:duration-300
+                    hover:bg-secondary hover:text-white rounded-lg p-4 text-center w-3/4 lg:w-1/2'>
+                        <div>
+                            <img src={img1} alt="Web Development" className='mx-auto' />
+                        </div>
+                        <h1 className='font-bold text-xl my-2'>Website Design</h1>
+                        <p>I create and design site with unique ideas and also use figma</p>
+                    </div>
+
+                    <div className='border hover:border-secondary border-accent 
+                    hover:transition-all hover:ease-linear
+                    hover:duration-300
+                    hover:bg-secondary hover:text-white rounded-lg p-4 text-center w-3/4 lg:w-1/2'>
+                        <div>
+                            <img src={img2} alt="Web Development" className='mx-auto' />
+                        </div>
+                        <h1 className='font-bold text-xl my-2'>Development</h1>
+                        <p>I build site go live with React, Firebase, MongoDB and more major technologies.</p>
+                    </div>
+
+                    <div className='border hover:border-secondary border-accent 
+                    hover:transition-all hover:ease-linear
+                    hover:duration-300
+                    hover:bg-secondary hover:text-white rounded-lg p-4 text-center w-3/4 lg:w-1/2'>
+                        <div>
+                            <img src={img3} alt="Web Development" className='mx-auto' />
+                        </div>
+                        <h1 className='font-bold text-xl my-2'>Graphic Design</h1>
+                        <p>I create abstract shapes using tools Ai,PhotoShop to design more interactive site.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Specializations;
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../Layouts/Main';
 import Home from '../Pages/Home/Home/Home';
+import About from '../Pages/Home/About/About';
 
 const router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
         {
           path: "/",
           element: <Home></Home>
+        },
+        {
+          path: "about",
+          element: <About></About>
         }
       ]
     },
